6ed63a32c19e0e246b0e9c1b9e7f31184e95a56e
Russell <russhwolf@gmail.com>
2021-04-25 15:43:04 -0400

    Simplify iOS app configuration

---
+++ iosApp/ToDo/ToDoApp.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import shared
+
+@main
+struct ToDoApp : App {
+        
+    var body: some Scene {
+        WindowGroup {
+            let repository: ToDoRepositoryIos = DependenciesIosKt.createRespository()
+            let publisher = createPublisher(repository.getList())
+                .map { $0.items.map { $0.toStruct() } }
+                .eraseToAnyPublisher()
+            ToDoList(
+                toDos: PublishedFlow(publisher, defaultValue: []),
+                onCreateItem: { repository.add(content: $0) },
+                onCheckedClick: { repository.toggleComplete(toDo: $0.toDataClass())},
+                onDeleteClick: { repository.remove(toDo: $0.toDataClass()) }
+            )
+        }
+    }
+}
