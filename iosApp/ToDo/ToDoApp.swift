0f6e543bb66e4acd359859dcce18d0320de582b0
russell <russhwolf@gmail.com>
2026-03-24 12:45:10 -0400

    Migrate off of experimental SKIE Observing view
    This helps show SKIE/Swift Export diff better

--- iosApp/ToDo/ToDoApp.swift
+++ iosApp/ToDo/ToDoApp.swift
@@ -5,16 +5,38 @@ import Shared
 struct ToDoApp : App {
 
     let repository: ToDoRepository = DependenciesIosKt.createToDoRepository()
-    
+
+    @State
+    var toDos: [ToDo] = []
+
     var body: some Scene {
         WindowGroup {
-            Observing(repository.getList(), initialContent: { ProgressView() }) { toDos in
-                ToDoList(
-                    toDos: toDos.map { $0.toStruct() },
-                    onCreateItem: { content in Task { try await repository.add(content: content) } },
-                    onCheckedClick: { toDo in Task { try await repository.toggleComplete(toDo: toDo.toDataClass() ) } },
-                    onDeleteClick: { toDo in Task { try await repository.remove(toDo: toDo.toDataClass() ) } }
-                )
+            ToDoList(
+                toDos: toDos,
+                onCreateItem: { content in
+                    Task {
+                        try await repository.add(content: content)
+                    }
+                },
+                onCheckedClick: { toDo in
+                    Task {
+                        try await repository.toggleComplete(toDo: toDo.toDataClass())
+                    }
+                },
+                onDeleteClick: { toDo in
+                    Task {
+                        try await repository.remove(toDo: toDo.toDataClass())
+                    }
+                }
+            )
+            .onAppear {
+                Task {
+                    for try await toDos in repository.getList() {
+                        self.toDos = toDos.map {
+                            $0.toStruct()
+                        }
+                    }
+                }
             }
         }
     }
