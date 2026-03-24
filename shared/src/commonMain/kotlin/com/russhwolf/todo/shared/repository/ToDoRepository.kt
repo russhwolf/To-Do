51a14002bf5e846e62096dde526d66ffe59dbeea
russell <russhwolf@gmail.com>
2026-03-24 13:13:49 -0400

    Remove superfluous CoroutineScope in repository

--- shared/src/commonMain/kotlin/com/russhwolf/todo/shared/repository/ToDoRepository.kt
+++ shared/src/commonMain/kotlin/com/russhwolf/todo/shared/repository/ToDoRepository.kt
@@ -1,13 +1,11 @@
 package com.russhwolf.todo.shared.repository
 
-import com.russhwolf.todo.shared.db.ToDo
-import com.russhwolf.todo.shared.db.ToDoDatabase
 import app.cash.sqldelight.coroutines.asFlow
 import app.cash.sqldelight.coroutines.mapToList
+import com.russhwolf.todo.shared.db.ToDo
+import com.russhwolf.todo.shared.db.ToDoDatabase
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
@@ -15,19 +13,17 @@ class ToDoRepository(
     private val database: ToDoDatabase,
     private val dispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
-    private val scope = CoroutineScope(SupervisorJob() + dispatcher)
-
     fun getList(): Flow<List<ToDo>> = database.toDoQueries.selectAll().asFlow().mapToList(dispatcher)
 
-    suspend fun add(content: String) = withContext(scope.coroutineContext) {
+    suspend fun add(content: String) = withContext(dispatcher) {
         database.toDoQueries.insertToDo(content)
     }
 
-    suspend fun remove(toDo: ToDo) = withContext(scope.coroutineContext) {
+    suspend fun remove(toDo: ToDo) = withContext(dispatcher) {
         database.toDoQueries.deleteById(toDo.id)
     }
 
-    suspend fun toggleComplete(toDo: ToDo) = withContext(scope.coroutineContext) {
+    suspend fun toggleComplete(toDo: ToDo) = withContext(dispatcher) {
         database.toDoQueries.updateComplete(!toDo.complete, toDo.id)
     }
 }
